51c08d97da657ca125418ded6bbc5e0efb391ee1
Dipta Das <dipta670@gmail.com>
2021-06-15 12:32:40 -0500

    Add test for running job

--- lib/jobs_test.go
+++ lib/jobs_test.go
@@ -91,3 +91,49 @@ func TestForceStop(t *testing.T) {
 		t.Fatalf("expected job error: %v, found: %v", expectedError, job.Error)
 	}
 }
+
+func TestRunningJobOutput(t *testing.T) {
+	jobWorker := NewJobWorker()
+
+	request := CreateJobRequest{
+		Command: "bash",
+		Args:    []string{"-c", "for i in {0..100}; do echo $i; sleep 0.5; done;"},
+	}
+
+	job, err := jobWorker.CreateJob(request)
+	if err != nil {
+		t.Fatalf("found error: %v", err)
+	}
+
+	// wait for generating some output
+	time.Sleep(time.Second)
+
+	expectedStatus := JobRunning
+
+	job, err = jobWorker.GetJobStatus(job.ID)
+	if err != nil {
+		t.Fatalf("found error: %v", err)
+	} else if job.Status != expectedStatus {
+		t.Fatalf("expected status: %v, found: %v", expectedStatus, job.Status)
+	} else if job.Output == "" {
+		t.Fatalf("found empty output")
+	}
+
+	oldOutput := job.Output
+	t.Log(oldOutput)
+
+	// get output again to ensure it contains the whole output, not just the unread part
+	// wait for generating some new output
+	time.Sleep(time.Second)
+
+	job, err = jobWorker.GetJobStatus(job.ID)
+	if err != nil {
+		t.Fatalf("found error: %v", err)
+	} else if job.Status != expectedStatus {
+		t.Fatalf("expected status: %v, found: %v", expectedStatus, job.Status)
+	} else if job.Output == "" || !strings.HasPrefix(job.Output, oldOutput) { // oldOutput should be prefix of current output
+		t.Fatalf("found empty output")
+	}
+
+	t.Log(job.Output)
+}
